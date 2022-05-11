d9fadedc9b1fc8ff629d9639415b405d8c551f7d
Rohit <rkg10072000@gmail.com>
2022-05-11 18:54:10 +0530

    implemented the basic create user logic

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,9 @@
-const register = (req, res) => {
-  res.send("register user");
+import { StatusCodes } from "http-status-codes";
+import User from "../models/user.js";
+
+const register = async (req, res) => {
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json(user);
 };
 
 const login = (req, res) => {
