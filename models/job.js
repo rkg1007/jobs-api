31796a5f44cff53863c9eb7a8888497bd9a88603
Rohit <rkg10072000@gmail.com>
2022-05-12 08:35:11 +0530

    created the job model

---
+++ models/job.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const JobSchema = mongoose.Schema({
+  company: {
+    type: String,
+    required: [true, "please provide company name"],
+    minlength: 3,
+    maxlength: 100,
+  },
+  position: {
+    type: String,
+    required: [true, "please provide position"],
+    minlength: 3,
+    maxlength: 50,
+  },
+  status: {
+    type: String,
+    enum: ["interview", "rejected", "pending", "selected"],
+    default: "pending",
+  },
+  createBy: {
+    type: mongoose.Types.ObjectId,
+    ref: "User"
+  }
+}, {
+  timestamps: true,
+});
+
+export default mongoose.model("Job", JobSchema);
