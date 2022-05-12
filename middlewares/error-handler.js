4c7b10eb8958b2d1631b7cd372d73dd713af24fc
Rohit <rkg10072000@gmail.com>
2022-05-12 10:47:43 +0530

    updated the error handler

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,13 +1,21 @@
 import { StatusCodes } from "http-status-codes";
-import CustomError from "../errors/custom-error.js";
 
 const errorHandler = (err, req, res, next) => {
-  let statusCode = StatusCodes.INTERNAL_SERVER_ERROR;
-  let msg = err.message;
+  let statusCode = err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR;
+  let msg = err.message || "something went wrong. please try again.";
 
-  if (err instanceof CustomError) {
-    statusCode = err.statusCode;
-    msg = err.message;
+  if (err.name) {
+    if (err.name === "ValidationError") {
+      msg = Object.values(err.errors).map(item => item.message).join(", ");
+    } else if (err.name == "CastError") {
+      msg = `given id: ${err.value} is not valid. please provide valid object id`;
+    }
+    statusCode = StatusCodes.BAD_REQUEST;
+  }
+
+  if (err.code || err.code === 11000) {
+    msg = `duplicate value for ${Object.keys(err.keyValue)}`;
+    statusCode = StatusCodes.BAD_REQUEST;
   }
 
   res.status(statusCode).json({
