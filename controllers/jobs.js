faf341222935235d4a3e064ae08a9651e2479dd7
Rohit <rkg10072000@gmail.com>
2022-05-12 10:09:03 +0530

    created crud functionality on job route

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,21 +1,51 @@
-const getAllJobs = (req, res) => {
-  res.send("get all jobs");
+import { StatusCodes } from "http-status-codes";
+import NotFound from "../errors/not-found.js";
+import BadRequest from "../errors/bad-request.js";
+import Job from "../models/job.js";
+
+const getAllJobs = async (req, res) => {
+  const jobs = await Job.find({ createdBy: req.user }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 };
 
-const getJob = (req, res) => {
-  res.send("get single job");
+const getJob = async (req, res) => {
+  const job = await Job.findOne({ _id: req.params.id, createdBy: req.user });
+  if (!job) {
+    throw new NotFound(`job with job id ${req.params.id} is not found`);
+  }
+  res.status(StatusCodes.OK).json({ ...job });
 };
 
-const createJob = (req, res) => {
-  res.send("create a job");
+const createJob = async (req, res) => {
+  const job = await Job.create({ ...req.body, createdBy: req.user });
+  res.status(StatusCodes.CREATED).json(job);
 };
 
-const updateJob = (req, res) => {
-  res.send("update the job");
+const updateJob = async (req, res) => {
+  const { company, position } = req.body;
+
+  if (!company || !position) {
+    throw new BadRequest("please provide company and position both.");
+  }
+
+  const job = await Job.findOneAndUpdate(
+    { _id: req.params.id, createdBy: req.user },
+    req.body,
+    { new: true }
+  );
+
+  if (!job) {
+    throw new NotFound(`job with job id ${req.params.id} is not found`);
+  }
+  res.status(StatusCodes.OK).json({ ...job });
 };
 
-const deleteJob = (req, res) => {
-  res.send("delete the job");
+const deleteJob = async (req, res) => {
+  const job = await Job.findOneAndDelete({ _id: req.params.id, createdBy: req.user });
+  if (!job) {
+    throw new NotFound(`job with job id ${req.params.id} is not found`);
+  }
+  res.status(StatusCodes.OK).json({ ...job });
 };
 
 export default {
